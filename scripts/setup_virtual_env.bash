f4c38b130c6e571731ae89e921f1a257842292a9
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2024-03-30 15:29:03 -0400

    Rearchitect repo to be a Python package (#5)

---
+++ scripts/setup_virtual_env.bash
@@ -0,0 +1,12 @@
+# Setup script for Python virtual environment.
+
+VIRTUALENV_PATH=".virtualenvs/pyroboplan"
+
+# Create a virtual environment, if one does not exist
+if [ ! -d "${VIRTUALENV_PATH}" ]; then
+    echo "Creating virtual environment"
+    python3 -m venv ${VIRTUALENV_PATH}
+fi
+
+echo "Sourcing virtual environment"
+source "${VIRTUALENV_PATH}/bin/activate"
