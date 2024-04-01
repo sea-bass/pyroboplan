8db78e8e3dd789778131aec986425eb1f95fc6f4
Sebastian Castro <sebas.a.castro@gmail.com>
2024-03-31 22:18:47 -0400

    Clean up visualize_frame

--- src/pyroboplan/visualization/meshcat_utils.py
+++ src/pyroboplan/visualization/meshcat_utils.py
@@ -2,28 +2,28 @@ import meshcat.geometry as mg
 import numpy as np
 
 
-def visualize_frame(visualizer, name, tform):
+FRAME_AXIS_POSITIONS = (
+    np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 0, 0], [0, 0, 1]])
+    .astype(np.float32)
+    .T
+)
+FRAME_AXIS_COLORS = (
+    np.array(
+        [[1, 0, 0], [1, 0.6, 0], [0, 1, 0], [0.6, 1, 0], [0, 0, 1], [0, 0.6, 1]]
+    )
+    .astype(np.float32)
+    .T
+)
 
-    FRAME_AXIS_POSITIONS = (
-        np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 0, 0], [0, 0, 1]])
-        .astype(np.float32)
-        .T
-    )
-    FRAME_AXIS_COLORS = (
-        np.array(
-            [[1, 0, 0], [1, 0.6, 0], [0, 1, 0], [0.6, 1, 0], [0, 0, 1], [0, 0.6, 1]]
-        )
-        .astype(np.float32)
-        .T
-    )
+def visualize_frame(visualizer, name, tform, line_length=0.2, line_width=3):
     visualizer.viewer[name].set_object(
         mg.LineSegments(
             mg.PointsGeometry(
-                position=0.2 * FRAME_AXIS_POSITIONS,
+                position=line_length * FRAME_AXIS_POSITIONS,
                 color=FRAME_AXIS_COLORS,
             ),
             mg.LineBasicMaterial(
-                linewidth=4,
+                linewidth=line_width,
                 vertexColors=True,
             ),
         )
