3c7e7231ffbb295fc7bec15da5a825dd80f130ca
Sebastian Castro <sebas.a.castro@gmail.com>
2024-04-04 09:49:42 -0400

    Fix coverage location

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -4,10 +4,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_RESULTS_DIR="${SCRIPT_DIR}/results"
+SRC_DIR="${SCRIPT_DIR}/../src"
 
 echo "Running unit tests..."
 python3 -m pytest "$SCRIPT_DIR" \
- --cov="$SCRIPT_DIR" --cov-branch --cov-report term \
+ --cov="$SRC_DIR" --cov-branch --cov-report term \
  --cov-report html:"$TEST_RESULTS_DIR/test_results_coverage_html" \
  --cov-report xml:"$TEST_RESULTS_DIR/test_results_coverage.xml" \
  --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
