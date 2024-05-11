2d62d5af7c0a0952d14915c6964b7a7f01364eae
Erik Holum <eholum@gmail.com>
2024-05-11 10:13:24 -0400

    Modify graph search tests

--- test/planning/test_graph_search.py
+++ test/planning/test_graph_search.py
@@ -3,27 +3,40 @@ from pyroboplan.planning.graph import Node, Graph
 from pyroboplan.planning.graph_search import astar, dfs
 
 
-def construct_graph(N):
-    """Creates a connected NxN graph for testing."""
-    g = Graph()
+def construct_square_graph(n):
+    """
+    Creates a connected n x n square graph for testing.
 
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
+    Each node in the graph will be connected to it's vertical and horizontal neighbor.
+    For example, for n == 3:
+
+    Graph
+    -----
+    (1,1) -- (1,2) -- (1,3)
+     |        |        |
+    (2,1) -- (2,2) -- (2,3)
+     |        |        |
+    (3,1) -- (3,2) -- (3,3)
+    """
+    graph = Graph()
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             node = Node([i, j])
-            g.add_node(node)
+            graph.add_node(node)
 
-    for node in g.nodes:
+    for node in graph.nodes:
         x, y = node.q[0], node.q[1]
         for px, py in [(x + 1, y), (x, y + 1)]:
             neighbor = Node([px, py])
-            if neighbor in g.nodes:
-                g.add_edge(node, neighbor)
+            if neighbor in graph.nodes:
+                graph.add_edge(node, neighbor)
 
-    return g
+    return graph
 
 
 def test_dfs():
-    g = construct_graph(5)
+    g = construct_square_graph(5)
 
     start_pose = Node([1, 1])
     goal_pose = Node([5, 5])
@@ -33,11 +46,13 @@ def test_dfs():
     assert path is not None
     assert path[0] == start_pose
     assert path[-1] == goal_pose
+    assert path[-1].cost == 8.0
+    assert len(path) == 9
 
 
 def test_astar():
     heuristic = lambda n1, n2: configuration_distance(n1.q, n2.q)
-    g = construct_graph(5)
+    g = construct_square_graph(5)
 
     start_pose = Node([1, 1])
     goal_pose = Node([5, 5])
