de017b3cf673f07e195aa7ce26ca196a3902f55b
Erik Holum <eholum@gmail.com>
2024-05-15 10:06:22 -0400

    Cleanup PRM example

--- examples/example_prm_2dof.py
+++ examples/example_prm_2dof.py
@@ -10,14 +10,37 @@ import time
 from pinocchio.visualize import MeshcatVisualizer
 
 from pyroboplan.core.utils import get_random_collision_free_state, get_random_state
-from pyroboplan.models.two_dof import (
-    load_models,
-)
 from pyroboplan.planning.prm import PRMPlanner, PRMPlannerOptions
 from pyroboplan.planning.utils import (
     discretize_joint_space,
     discretize_joint_space_path,
 )
+
+
+def load_2dof_model():
+
+    from pyroboplan.models.two_dof import (
+        load_models,
+    )
+
+    model, collision_model, visual_model = load_models()
+
+    return model, collision_model, visual_model, "ee"
+
+
+def load_panda_model():
+
+    from pyroboplan.models.panda import (
+        load_models,
+        add_self_collisions,
+        add_object_collisions,
+    )
+
+    model, collision_model, visual_model = load_models()
+    add_self_collisions(model, collision_model)
+    add_object_collisions(model, collision_model, visual_model)
+
+    return model, collision_model, visual_model, "panda_hand"
 
 
 def discretized_sample_generator(model, step_size):
@@ -36,8 +59,8 @@ def discretized_sample_generator(model, step_size):
         yield get_random_state(model)
 
 
-def run_prm_search(q_start, q_end, planner, options):
-    for i in range(5):
+def run_prm_search(q_start, q_end, planner, options, ee_name, max_retries=5):
+    for i in range(max_retries):
         print(f"Searching for path attempt: {i+1}...")
         path = planner.plan(q_start, q_end)
         if path:
@@ -46,11 +69,11 @@ def run_prm_search(q_start, q_end, planner, options):
         else:
             print("Failed to find a path, growing the PRM...")
             planner.construct_roadmap()
-            planner.visualize(viz, "ee", show_path=False, show_graph=True)
+            planner.visualize(viz, ee_name, show_path=False, show_graph=True)
 
     # Animate the path
     if path:
-        planner.visualize(viz, "ee", show_path=True, show_graph=False)
+        planner.visualize(viz, ee_name, show_path=True, show_graph=False)
         input("Press 'Enter' to animate the path.")
         for idx in range(1, len(path)):
             segment_start = path[idx - 1]
@@ -64,9 +87,10 @@ def run_prm_search(q_start, q_end, planner, options):
 
 
 if __name__ == "__main__":
-    # Create models and data
-    model, collision_model, visual_model = load_models()
-
+    # Default to the simple 2 DOF example model. If switching to the Panda
+    # or another high DOF manipulator, we recommend increasing the max_neighbor_radius
+    # and max_neighbor_connections to accelerate planning.
+    model, collision_model, visual_model, ee_name = load_2dof_model()
     data = model.createData()
     collision_data = collision_model.createData()
 
@@ -105,17 +129,18 @@ if __name__ == "__main__":
 
     # Visualize the resulting PRM
     print("Visualizing the PRM...")
-    planner.visualize(viz, "ee", show_path=False, show_graph=True)
+    planner.visualize(viz, ee_name, show_path=False, show_graph=True)
 
     # We can save the graph to file for use in future PRMs.
     # planner.graph.save_to_file("a.graph")
 
     while True:
-        run_prm_search(q_start, q_end, planner, options)
+        run_prm_search(q_start, q_end, planner, options, ee_name, max_retries=5)
 
         # Reusing planners for repeated queries in static environments is one of the
         # key advantages of PRMs.
         input("Press 'Enter' to plan another trajectory, or ctrl-c to quit.")
+        print()
         planner.reset()
         q_start = q_end
         q_end = get_random_collision_free_state(model, collision_model)
